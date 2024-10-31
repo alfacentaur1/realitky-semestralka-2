53ba392cc4dd2f43fe639266cdb74369bafe408f
Filip <filip1kop@gmail.com>
2024-10-31 08:08:49 +0100

    pridany javascript - zavislost poli na sign upformulari

--- js/script.js
+++ js/script.js
@@ -14,14 +14,38 @@ function isEmailSubmittable(e) {
         e.preventDefault();
         test.classList.add("chyba");
     }
-
 }
 
+//shoduji se hesla?
+function arePasswordsSame(e) {
+    let pas1 = document.getElementById("password");
+    let pas2 = document.getElementById("password-znovu");
+
+    if (pas1.value === pas2.value) {
+        //vse ok
+    }
+
+    else {
+        e.preventDefault();
+        let p = document.getElementById("error_hesla");
+        p.classList.add("error_hesla");
+        pas1.classList.add("password")
+        pas2.classList.add("password")
+    }
+}
+
+function isFormSubmittable(e) {
+    isEmailValid(e);
+    arePasswordsSame(e);
+}
 
 function main() {
     let email = document.querySelector("[name=email]");
     email.addEventListener("blur",isEmailValid);
-    email.addEventListener("submit", isEmailSubmittable)
+
+    let form = document.querySelector("form");
+    form.addEventListener("submit",isFormSubmittable);
+
 }
 
 main();
